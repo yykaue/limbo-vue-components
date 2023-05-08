88c3140eadf3f3b70e236e80ef374be92df59155
limbo <yykaue@qq.com>
2023-05-08 11:38:24 +0800

    chore: 增加eslint修复脚本

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-confusing-v-for-v-if': 'error'
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ],
+
+  'extends': [
+    'plugin:vue/strongly-recommended',
+    '@vue/standard'
+  ]
+}
