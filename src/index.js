332605b24f171bcf9677ca6ff37ad13b7c9f5ba5
limbo <yykaue@qq.com>
2023-07-27 15:23:27 +0800

    feat: 增加第三方库和组件UI注册方式

--- src/index.js
+++ src/index.js
@@ -38,36 +38,38 @@ const components = {
   // KeVideo
 }
 
-const install = (Vue, options = {}) => {
-  // limbo-common-css
-  // TODO 是否需提示开发者需要注入第三方UI插件
-  // const ElementUI = [
-  //   'Button',
-  //   'Checkbox',
-  //   'CheckboxGroup',
-  //   'Col',
-  //   'DatePicker',
-  //   'Dialog',
-  //   'Form',
-  //   'FormItem',
-  //   'Input',
-  //   'InputNumber',
-  //   'Menu',
-  //   'MenuItem',
-  //   'Option',
-  //   'Pagination',
-  //   'Progress',
-  //   'Table',
-  //   'TableColumn',
-  //   'Tooltip',
-  //   'Radio',
-  //   'RadioGroup',
-  //   'Row',
-  //   'Scrollbar',
-  //   'Select',
-  //   'Submenu',
-  //   'Switch'
-  // ]
+// limbo-common-css
+// TODO 是否需提示开发者需要注入第三方UI插件
+const componentUI = [
+  'Button',
+  'Checkbox',
+  'CheckboxGroup',
+  'Col',
+  'DatePicker',
+  'Dialog',
+  'Form',
+  'FormItem',
+  'Input',
+  'InputNumber',
+  'Menu',
+  'MenuItem',
+  'Option',
+  'Pagination',
+  'Progress',
+  'Table',
+  'TableColumn',
+  'Tooltip',
+  'Radio',
+  'RadioGroup',
+  'Row',
+  'Scrollbar',
+  'Select',
+  'Submenu',
+  'Switch'
+]
+
+const install = async (Vue, options = {}) => {
+  const { componentUI, libraryUI } = options
   const payloadList = ['tools', 'Message', 'MessageBox']
 
   payloadList.forEach(item => {
@@ -81,6 +83,17 @@ const install = (Vue, options = {}) => {
       printInformation(infoArray)
     }
   })
+
+  if (componentUI) {
+    componentUI.forEach(component => {
+      Vue.component(component.name, component)
+    })
+  }
+  if (libraryUI) {
+    libraryUI.forEach(library => {
+      Vue.use(library)
+    })
+  }
 
   Vue.prototype.$tools = Object.assign({},
     Vue.prototype.$tools,
@@ -108,10 +121,12 @@ export {
   KeTable,
   KeToBack,
   KeUnfinished,
-  KeUploadAli
+  KeUploadAli,
   // KeVideo
+  componentUI
 }
 export default {
   install,
+  componentUI,
   ...components
 }
